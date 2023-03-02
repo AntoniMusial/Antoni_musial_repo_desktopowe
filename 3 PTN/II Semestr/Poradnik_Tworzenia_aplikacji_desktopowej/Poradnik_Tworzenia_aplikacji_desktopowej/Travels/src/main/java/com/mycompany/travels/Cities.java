e42c86e4e80c86a513251ff5802e2ce9ccf9fac1
musialantoni <antoni.glowny@gmail.com>
2023-03-02 13:55:59 +0100

    aktualizacja

---
+++ 3 PTN/II Semestr/Poradnik_Tworzenia_aplikacji_desktopowej/Poradnik_Tworzenia_aplikacji_desktopowej/Travels/src/main/java/com/mycompany/travels/Cities.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.travels;
+
+/**
+ *
+ * @author musialantoni
+ */
+public class Cities {
+    void cities() {
+        for (int i=0; i<5; i++) {
+        }
+    }
+}
